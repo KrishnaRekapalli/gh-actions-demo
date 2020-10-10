a5b97d0fdfaff510d97e51a18fd1d1bdee3ffdc7
Krishna Teja Rekapalli <krrish1729@gmail.com>
2020-10-10 10:25:41 -0500

    Update rt_mumbai.py

--- rt_mumbai.py
+++ rt_mumbai.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import arviz as az
 from matplotlib import pyplot as plt
-from rtmoddles.generative import GenerativeModel
+from rtmoddel.generative import GenerativeModel
 %config InlineBackend.figure_format = 'retina'
 
 
