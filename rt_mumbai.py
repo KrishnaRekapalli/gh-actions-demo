349480c10101889dda02d3cca0539d07a93b2aab
Krishna Teja Rekapalli <krrish1729@gmail.com>
2020-10-10 10:24:48 -0500

    Create rt_mumbai.py

---
+++ rt_mumbai.py
@@ -0,0 +1,11 @@
+import pymc3 as pm
+import pandas as pd
+import numpy as np
+import arviz as az
+from matplotlib import pyplot as plt
+from rtmoddles.generative import GenerativeModel
+%config InlineBackend.figure_format = 'retina'
+
+
+
+
